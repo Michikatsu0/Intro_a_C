40cef5246a9e97feb0712e30e3d5d51e04e71273
Sebastian Segovia <sebastian.segovia@upb.edu.co>
2022-02-22 11:51:23 -0500

    5

--- Ejercicio_3/main.c
+++ Ejercicio_3/main.c
@@ -1,9 +1,11 @@
+ #include <stdio.h>
+ 
 #define M 5
 #define C 5
 
 int main(int argc, char* argv[]) {
     int x = 2;
     int y = x*M + C;
-    printf(y);
+    printf("y: %d \n",y);
     return 0;
 }
